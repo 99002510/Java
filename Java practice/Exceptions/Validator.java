b4c92d3d0f902b095a9512d517afb5b90a7113bb
99002510 <71481606+99002510@users.noreply.github.com>
2020-10-20 09:29:43 +0530

    Update Validator.java

--- Java practice/Exceptions/Validator.java
+++ Java practice/Exceptions/Validator.java
@@ -3,21 +3,28 @@ import com.Exceptions.NameExistsException;
 import com.Exceptions.TooLongException;
 import com.Exceptions.TooShortException;
 
-public class Validator {
+public class Validator 
+{
 	private String[] names = {"Ankith","Raju","Nipun","Rohan"};
-	public boolean validateName(String username) throws NameExistsException {
-		for(String uname:names) {
-			if(uname.equals(username)) {
+	public boolean validateName(String username) throws NameExistsException 
+	{
+		for(String uname:names) 
+		{
+			if(uname.equals(username)) 
+			{
 				throw new NameExistsException();
 			}
 		}
 		return true;
 	}
-	public boolean validatePassword(String password) throws TooLongException,TooShortException {
-		if (password.length()>=15) {
+	public boolean validatePassword(String password) throws TooLongException,TooShortException 
+	{
+		if (password.length()>=15) 
+		{
 			throw new TooLongException("Password is too Lomg");
 		}
-		if(password.length()<=5) {
+		if(password.length()<=5) 
+		{
 			throw new TooShortException("Password is too short");
 		}
 		return true;
