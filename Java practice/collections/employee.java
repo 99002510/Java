281fe4e39592e2d78e0ab2716cba651dc094c4e2
99002510 <71481606+99002510@users.noreply.github.com>
2020-10-20 09:13:13 +0530

    Update employee.java

--- Java practice/collections/employee.java
+++ Java practice/collections/employee.java
@@ -1,44 +1,55 @@
 package com.collections;
 
-public class Employee implements Comparable<Employee>{
+public class Employee implements Comparable<Employee>
+{
 	private String empname;
 	private String empcity;
 	
-	public Employee() {
+	public Employee() 
+	{
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public Employee(String empname, String empcity, Integer empId) {
+	public Employee(String empname, String empcity, Integer empId) 
+	{
 		super();
 		this.empname = empname;
 		this.empcity = empcity;
 		this.empId = empId;
 	}
 	@Override
-	public String toString() {
+	public String toString() 
+	{
 		return "Employee [name=" + empname + ", city=" + empcity + ", empId=" + empId + "]";
 	}
 	private Integer empId;
-	public String getName() {
+	public String getName() 
+	{
 		return empname;
 	}
-	public void setName(String empname) {
+	public void setName(String empname) 
+	{
 		this.empname = empname;
 	}
-	public String getCity() {
+	public String getCity() 
+	{
 		return empcity;
 	}
-	public void setCity(String empcity) {
+	public void setCity(String empcity) 
+	{
 		this.empcity = empcity;
 	}
-	public Integer getEmpId() {
+	public Integer getEmpId() 
+	{
 		return empId;
 	}
-	public void setEmpId(Integer empId) {
+	public void setEmpId(Integer empId) 
+	{
 		this.empId = empId; 
 	}
 	@Override
-	public int compareTo(Employee employee) {
+	public int compareTo(Employee employee) 
+	{
 		// TODO Auto-generated method stub
 		return this.getName().compareTo(employee.getName());
 	}
