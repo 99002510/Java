c7eaf25728738b558b223c3fc81d5f15b2085022
99002510 <71481606+99002510@users.noreply.github.com>
2020-10-20 09:17:26 +0530

    Update sort_demo.java

--- Java practice/collections/sort_demo.java
+++ Java practice/collections/sort_demo.java
@@ -4,45 +4,54 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
-public class sortDemo {
-	public static void main(String args[]) {
-		Employee employee1 = new Employee("Pradeep","Mysore",122);
-		Employee employee2 = new Employee("Rama","Banglore",124);
-		Employee employee3 = new Employee("Ravi","Manglore",125);
-		Employee employee4 = new Employee("Raman","Mandya",126);
+public class sortDemo 
+{
+	public static void main(String args[]) 
+	{
+		Employee employee1 = new Employee("Saptha","Hassan",101);
+		Employee employee2 = new Employee("Rama","Banglore",102);
+		Employee employee3 = new Employee("Ravi","Manglore",103);
+		Employee employee4 = new Employee("Raman","Mandya",104);
 		ArrayList<Employee> employeeList = new ArrayList<>();
 		employeeList.add(employee1);
 		employeeList.add(employee2);
 		employeeList.add(employee3);
 		employeeList.add(employee4);
 		employeeList.add(new Employee("Akashay","Jammu",111));
-		for(Employee emp1 : employeeList) {
+		for(Employee emp1 : employeeList) 
+		{
 			System.out.println(emp1);
 		}
 		Collections.sort(employeeList, new Namesort());
 		System.out.println("Sorted by Name");
-		for(Employee emp1 : employeeList) {
+		for(Employee emp1 : employeeList) 
+		{
 			System.out.println(emp1);
 		}
-		Collections.sort(employeeList, new Comparator<Employee>() {
+		Collections.sort(employeeList, new Comparator<Employee>() 
+		{
 
 			@Override
-			public int compare(Employee emp1, Employee emp2) {
+			public int compare(Employee emp1, Employee emp2) 
+			{
 				// TODO Auto-generated method stub
 				return emp1.getCity().compareTo(emp2.getCity());
 			}
 			
 		});
 		System.out.println("Sorted by City");
-		for(Employee emp1 : employeeList) {
+		for(Employee emp1 : employeeList) 
+		{
 			System.out.println(emp1);
 		}
-		Collections.sort(employeeList, (emp1,emp2)->{
+		Collections.sort(employeeList, (emp1,emp2)->
+		{
 			return emp1.getEmpId().compareTo(emp2.getEmpId());
 			
 		});
 		System.out.println("Sorted by Employee");
-		for(Employee emp1 : employeeList) {
+		for(Employee emp1 : employeeList) 
+		{
 			System.out.println(emp1);
 		}
 		
