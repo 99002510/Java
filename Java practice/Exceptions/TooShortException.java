97b0396b090b5fa8428feb10ec2158a86df0b7c8
99002510 <71481606+99002510@users.noreply.github.com>
2020-10-20 09:29:03 +0530

    Update TooShortException.java

--- Java practice/Exceptions/TooShortException.java
+++ Java practice/Exceptions/TooShortException.java
@@ -1,21 +1,20 @@
 package com.Exceptions;
 
-public class TooShortException extends Exception {
-
+public class TooShortException extends Exception 
+{
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public TooShortException() {
+	public TooShortException() 
+	{
 		// TODO Auto-generated constructor stub
 	}
 
-	public TooShortException(String message) {
+	public TooShortException(String message) 
+	{
 		super(message);
 		// TODO Auto-generated constructor stub
 	}
-
-
-
 }
