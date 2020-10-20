3667b0fa2772fd1136fa31315be509409acc84ee
99002510 <71481606+99002510@users.noreply.github.com>
2020-10-20 09:08:17 +0530

    Update namesort.java

--- Java practice/collections/namesort.java
+++ Java practice/collections/namesort.java
@@ -2,13 +2,15 @@ package com.collections;
 
 import java.util.Comparator;
 
-public class Namesort implements Comparator<Employee> {
+public class Namesort implements Comparator<Employee> 
+{
 
-@Override
-public int compare(Employee emp1, Employee emp2) {
-  // TODO Auto-generated method stub
-	return emp1.getName().compareTo(emp2.getName());
-  }
+	@Override
+	public int compare(Employee emp1, Employee emp2) 
+	{
+ 		// TODO Auto-generated method stub
+		return emp1.getName().compareTo(emp2.getName());
+  	}
 	
 
 }
